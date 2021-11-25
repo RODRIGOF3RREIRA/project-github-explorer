e7d461156871a76d6ce140f8e30dc1c30900127b
Rodrigo Ferreira de Paiva <rodrigoferreira2799@gmail.com>
2021-11-25 13:08:23 -0400

    Aula 01

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+import { SearchBar } from "./components/SearchBar";
+import { Container } from "./styles/Home";
+function App() {
+  return (
+    <Container>
+      <SearchBar />;
+      <GlobalStyle />;
+    </Container>
+  );
+}
+
+export default App;
